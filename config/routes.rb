238f3e3d871569f1e87390af8ca55c926c142982
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-21 19:47:21 +0900

    🚚  update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get '/' => 'home#top'
+  
+  get 'home/top'
+  # [controller#action]
+  # get 'home/top' => 'home#top'
+
+  get 'top' => 'home#top'
+  get 'home/about' => 'home#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
