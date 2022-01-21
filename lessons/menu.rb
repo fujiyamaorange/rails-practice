176f032a684db87af6899288eee21746b9e3acac
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-21 15:32:55 +0900

    🚧  add menu sample

---
+++ lessons/menu.rb
@@ -0,0 +1,18 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+
+  def initialize(name:, price:)
+    self.name = name
+    self.price = price
+  end
+  
+  def info
+    return "#{self.name} #{self.price}円"
+  end
+
+  def get_total_price(count)
+    return count * self.price
+  end
+
+end
