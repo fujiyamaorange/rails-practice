6290e33073ca657eb5c44eb03c6750de63ddca68
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-22 14:55:05 +0900

    ✨ add create controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,22 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
     # paramsから取得できる
     @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    # POSTで渡ってきたデータをデータベースに保存しリダイレクトする
+    # 変数名は@なしでもいける
+    @new_post = Post.new(content: params[:content]) 
+    @new_post.save
+
+    redirect_to("/posts/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  get 'posts/new' => 'post@new'
   get 'posts/:id' => 'posts#show'
   get '/' => 'home#top'
   
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'top' => 'home#top'
   get 'home/about' => 'home#about'
+
+  post 'posts/create' => 'posts#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
