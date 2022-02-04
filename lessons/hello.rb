872da658f6e072ad3e00730aab89795f8bac96d9
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-04 15:02:06 +0900

    👽️ get parameters

--- lessons/hello.rb
+++ lessons/hello.rb
@@ -4,7 +4,20 @@ class HelloAPI < Grape::API
   format :json
 
   get '/' do
-    {'hello': 'world', 'params': params}
+    headers = headers['X-Account-Id']
+    { headers: headers, params: params }
+  end
+
+  # queryパラメータ
+  get '/query' do
+    query = params[:name]
+    return { query: query }
+  end
+  
+  # pathパラメータ
+  get 'users/:user_id' do
+    user_id = params[:user_id]
+    return { user_id: user_id }
   end
 
   post '/' do
