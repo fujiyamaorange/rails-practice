6743d8d5487d21d1b545b7192b8816f3a3131f3d
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-19 17:42:53 +0900

    ✅ use subject

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,9 @@ RSpec.describe User, type: :model do
   describe '#posts' do
     # letは実行されるときに呼び出される
     let(:params) {{ name: 'kaito', email: 'kaito@gmail.com'}}
-    it 'ユーザーを作成' do
-      user = User.new(params)
-      expect(user.posts).to eq []
-    end
+    let(:user) { User.new(params)}
+    subject { user.posts }
+
+    it { is_expected.to eq [] }
   end
-  
-  
 end
