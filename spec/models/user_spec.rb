e49a9e139c23967a24f1c316b7359411aff62ebe
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-19 17:04:35 +0900

    ✅ add example test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it '1 + 1 = 2であること' do
+    expect(1 + 1).to eq 2
+  end
+  it '10 - 9 = 1であること' do
+    expect(10 - 9).to eq 1
+  end
+end
