6c0be865bfa7108650180ee9dada8dd241baaf36
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-19 17:28:57 +0900

    ✅ use let

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it '#posts' do
-    user = User.new(name: 'kaito', email: 'kaito@gmail.com')
-    expect(user.posts).to eq []
+  describe '#posts' do
+    # letは実行されるときに呼び出される
+    let(:params) {{ name: 'kaito', email: 'kaito@gmail.com'}}
+    it 'ユーザーを作成' do
+      user = User.new(params)
+      expect(user.posts).to eq []
+    end
   end
+  
+  
 end
