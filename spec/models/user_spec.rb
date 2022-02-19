b3727de6ba0513776afc6dc8cd2281114407f0ab
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-19 17:16:47 +0900

    ✅ update test for using model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it '1 + 1 = 2であること' do
-    expect(1 + 1).to eq 2
-  end
-  it '10 - 9 = 1であること' do
-    expect(10 - 9).to eq 1
+  it '#posts' do
+    user = User.new(name: 'kaito', email: 'kaito@gmail.com')
+    expect(user.posts).to eq []
   end
 end
