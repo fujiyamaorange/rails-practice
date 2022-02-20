0f688047714f0b0fe904261e97e9b2910d94cef1
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-20 09:55:54 +0900

    ✅ use pending

---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "UsersController", type: :request do
+  # describe 'GET#index' do
+  # end
+
+  # pendingされる
+  xit 'absolute success' do
+    # pending '以下は失敗するのでpending'
+    expect(1 + 1).to eq 3
+  end
+end
