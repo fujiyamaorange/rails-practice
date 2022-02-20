42e8bc810ae82fdf7be1f7be70a7373168794655
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-20 10:34:04 +0900

    💩 need to make auth helper

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "UsersController", type: :request do
-  # describe 'GET#index' do
-  # end
+  describe 'GET #index' do
+    it 'returns a 302 response' do
+      get '/users/index'
+      expect(response).to have_http_status "302"
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      @user = User.new({name: 'kaito', email: 'kaito@gmail.com'})
+    end
+    it 'responses successfully' do
+      get '/users/:id', params: {id: @user.id}
+      expect(response).to have_http_status "302"
+    end
+  end
+  
 
   # pendingされる
   xit 'absolute success' do
